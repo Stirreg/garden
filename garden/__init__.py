5abff73b47c26ae313781a1476cd2dde76165a88
Erik Gerrits <ghrgerrits@gmail.com>
2017-09-21 21:33:24 +0200

    Restructured app. Made further preperations for running on uwsgi.

--- garden/__init__.py
+++ garden/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MONGO_CONNECT'] = False
+
 import garden.views.plant
