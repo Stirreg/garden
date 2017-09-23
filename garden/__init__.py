1d2ce384ab635184c233fc4c6c44db67b8a35447
Erik Gerrits <ghrgerrits@gmail.com>
2017-09-23 11:48:14 +0200

    Added docker configuration.

--- garden/__init__.py
+++ garden/__init__.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['MONGO_CONNECT'] = False
+app.config['MONGO_HOST'] = 'mongodb'
 
 import garden.views.plant
